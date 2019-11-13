56dcf3973f67dba6c747e71e19590474b0bc7c09
Mario Alberto Barragán Espinosa <>
2019-11-12 20:44:58 -0600

    Can add activities, save data on userDefaults

---
+++ Habit-tracking/AddActivity.swift
@@ -0,0 +1,21 @@
+//
+//  AddActivity.swift
+//  Habit-tracking
+//
+//  Created by Mario Alberto Barragán Espinosa on 12/11/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddActivity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddActivity_Previews: PreviewProvider {
+    static var previews: some View {
+        AddActivity()
+    }
+}
