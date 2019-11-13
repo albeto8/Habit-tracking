43bd0958a5f7bcfa4f6264dcfb7163cc66a74912
Mario Alberto Barragán Espinosa <>
2019-11-12 19:51:17 -0600

    First commit

---
+++ Habit-tracking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habit-tracking
+//
+//  Created by Mario Alberto Barragán Espinosa on 12/11/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
